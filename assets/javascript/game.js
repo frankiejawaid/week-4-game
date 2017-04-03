8b588058d91a1a868bebd605b83436d16a732e52
frankiejawaid <frankiejawaid@gmail.com>
2017-04-02 22:44:14 -0500

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,105 +1,78 @@
+$(document).ready(function() {
 
-details = {
-	score: 0,
-	wins: 0,
-	losses: 0,
+	crystals = ['assets/images/red.png','assets/images/blue.png','assets/images/yellow.png','assets/images/green.png'];
 
-	random_number: function(){
-			return (Math.floor(Math.random()*102)) + 19;
-	},
+	var counter = 0;
+	var wins = 0;
+	var losses = 0;
+	$('#win').text(wins);
+	$('#loss').text(losses);
+	
+	newCrystals();
+	newGame();
 
-	random_number_gem:function(){
-			return (Math.floor(Math.random()*12)) +1;
+	function newCrystals () {
+		var numbers = []
+			while(numbers.length < 4){
+			  var randomnumber = Math.ceil(Math.random()*12)
+			  var found = false;
+			  for (var i=0; i< numbers.length; i++){
+				if (numbers[i] == randomnumber){
+					found = true; break
+				}
+			  }
+			  if(!found)numbers[numbers.length]=randomnumber;
+			}
+		console.log(numbers);		
+
+		for (i = 0; i < numbers.length; i++) {
+			var imageCrystal = $('<img>');
+			imageCrystal.attr('data-num', numbers[i]);
+			imageCrystal.attr('src', crystals[i]);
+			imageCrystal.attr('alt', 'crystals');
+			imageCrystal.addClass('crystalImage')
+			$('#crystals').append(imageCrystal);
+		}
 	}
-}
+
+	function newGame() {
+
+		counter = 0;
+		$('#yourScore').text(counter);
+
+		function randomIntFromInterval(min,max){
+		   	return Math.floor(Math.random()*(max-min+1)+min);
+			}
+
+		var numberToGuess = randomIntFromInterval(19,120);
+
+		$('.value').text(numberToGuess);
 
 
- var random_guess = game.random_number();
- var guess1 = game.random_number_gem();
- var guess2 = game.random_number_gem();
- var guess3 = game.random_number_gem();
- var guess4 = game.random_number_gem();
- 	$(document).ready(function(){
- 		//// $(#wins).html(game.wins);
- 		//// $(#losses).html(game.losses);
- 	
- 	tracking_score = {
+		$('.crystalImage').on('click', function(){
+		    counter = counter + parseInt($(this).data('num'));
+		   
+		    $('#yourScore').text(counter);
 
- 		//winning
- 				function(){
- 					if (game.score === random_guess){
- 						alert("YOU WIN!!!");
- 						game.wins = game.wins +1;
- 						console.log(wins);
- 						game.score = 0;
- 						random_guess = game.random_number();
- 						guess1 = game.random_number_gem();
- 						guess2 = game.random_number_gem();
- 						guess3 = game.random_number_gem();
- 						guess4 = game.random_number_gem();
- 						$("#score").html(0);
- 						$("#wins").html("wins:" + game.wins);
- 						$("#random_number").html(random_guess);
- 					}
- 				},
- 		
- 		//losing
- 				function(){
- 					if (game.score > random_guess){
- 						alert("YOU LOSE!!!");
- 						game.losses = game.losses +1;
- 						console.log(losses);
- 						game.score = 0;
- 						randomguess= game.randomguess();
- 						guess1 = game.randomnumber_gem();
- 						guess2 = game.randomnumber_gem();
- 						guess3 = game.randomnumber_gem();
- 						guess4 = game.randomnumber_gem();
- 						$("#score").html(0);
- 						$("#losses").html("losses:" + game.losses);
- 						$("#randomnumber").html(randomguess);
+		    if (counter == numberToGuess){
+		      alert("YOU LOSE!!!");
+		      wins ++;
+		      $('#win').text(wins);
+		      console.log(wins)
+		      $('#crystals').empty();
+		      newCrystals();
+		      newGame();
+		        
+		    } else if ( counter > numberToGuess){
+		        $('#status').text('You lost!')
+		        losses ++;
+		        $('#loss').text(losses);
+		        console.log(losses)
+		        $('#crystals').empty();
+		        newCrystals();
+		        newGame();
+		    }
+		});
+	}
 
- 					}
- 				}
-
- 			}
-
- 			//Generating red random
- 				$("#randomnumber").html(randomguess);
- 				$("red").on("click",function(){
- 					game.score +- guess1;
- 					$("#score").html(game.score);
- 					tracking_score.winning();
- 					tracking_score.losing();
- 				});
-
- 				
- 			
- 			//generating blue random
- 				$("#randomnumber").html(randomguess);
- 				$("blue").on("click",function(){
- 					game.score +- guess2;
- 					$("#score").html(game.score);
- 					tracking_score.winning();
- 					tracking_score.losing();
- 				});
-
- 			//Generating yellow random
- 				$("#randomnumber").html(randomguess);
- 				$("yellow").on("click",function(){
- 					game.score +- guess3;
- 					$("#score").html(game.score);
- 					tracking_score.winning();
- 					tracking_score.losing();
- 				});	
-
- 			//Generating green random
- 				$("#randomnumber").html(randomguess);
- 				$("green").on("click",function(){
- 					game.score +- guess4;
- 					$("#score").html(game.score);
- 					tracking_score.winning();
- 					tracking_score.losing();
- 				});
-
- 			});
+});
